1611200ae72f70383b388ed3972ffaafe36c7ce0
Erich <95936922+erich-ika@users.noreply.github.com>
2024-04-04 17:05:27 -0300

    teste1

--- teste1/namorando.js
+++ teste1/namorando.js
@@ -1,7 +1,47 @@
 function buscar(cb) {
-    // TODO simula buscar 4 casais do banco de dados, demorando 2 segundos
+    // TODO simula buscar 12 casais do banco de dados, demorando 2 segundos
     setTimeout(() => {
         const mockCasais = [
+            [
+                {
+                    id: "guido",
+                    name: "Guido",
+                },
+                {
+                    id: "abigail",
+                    name: "Abigail",
+                },
+            ],
+            [
+                {
+                    id: "pancho",
+                    name: "Pancho",
+                },
+                {
+                    id: "kiwi",
+                    name: "Kiwi",
+                },
+            ],
+            [
+                {
+                    id: "bingo",
+                    name: "Bingo",
+                },
+                {
+                    id: "jade",
+                    name: "Jade",
+                },
+            ],
+            [
+                {
+                    id: "lennon",
+                    name: "Lennon",
+                },
+                {
+                    id: "jujuba",
+                    name: "Jujuba",
+                },
+            ],
             [
                 {
                     id: "guido",
@@ -29,35 +69,41 @@ function buscar(cb) {
 
 function render(casais) {
     divpetsnamorando = document.getElementById("petsnamorando");
+
     casais.forEach((casal, i) => {
-        if ((i - 2) % 4 == 0) {
+        divcasal = document.createElement("div");
+        divcasal.classList = "casal";
+
+        if (i % 6 == 0) {
             let divpublicidade = document.createElement("div");
             divpublicidade.classList = "publicidade";
             divpublicidade.innerText = "Publicidade 315x80";
-            divpetsnamorando.appendChild(divpublicidade);
+            divcasal.appendChild(divpublicidade);
         }
 
-        let divcasal = document.createElement("div");
-        divcasal.className = "casal";
+        let divcasalpfp = document.createElement("div");
+        divcasalpfp.className = "casalpfp";
 
         casal.forEach((pet) => {
             let imgpfp = document.createElement("img");
             imgpfp.className = "pfp";
             imgpfp.src = `./assets/namorando/pfp/${pet.id}.png`;
             imgpfp.alt = pet.name;
-            divcasal.appendChild(imgpfp);
+            divcasalpfp.appendChild(imgpfp);
         });
 
         let imgheart = document.createElement("img");
         imgheart.classList = "heart";
         imgheart.src = "./assets/namorando/heart.svg";
-        divcasal.appendChild(imgheart);
+        divcasalpfp.appendChild(imgheart);
 
-        divpetsnamorando.appendChild(divcasal);
+        divcasal.appendChild(divcasalpfp);
 
         let h3casalname = document.createElement("h3");
         h3casalname.innerText = `${casal[0].name} e ${casal[1].name}`;
-        divpetsnamorando.appendChild(h3casalname);
+        divcasal.appendChild(h3casalname);
+
+        divpetsnamorando.appendChild(divcasal)
     });
 
     document.getElementById("carregar-mais").classList.remove("loading");
